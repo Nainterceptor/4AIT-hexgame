3a50b433ab4158fe442594c970630dbbf0fc67f3
Robin Lebert <robin.lebert@gmail.com>
2015-03-13 18:07:40 +0100

    add path Ai

--- hex.js
+++ hex.js
@@ -100,8 +100,182 @@
         };
     }]);
 
-    hex.service('PlayService',['InfosService','PlayRandomService','GridService','WinService',
-        function(InfosService,PlayRandomService,GridService,WinService){
+    hex.service('PlayPathService',['InfosService','GridService',function(InfosService,GridService){
+        var bluePreviousCells = [];
+        var redPreviousCells = [];
+
+        this.init = function(){
+          bluePreviousCells = [];
+            redPreviousCells = [];
+        };
+
+        var isPlaybable = function(l,c){
+            var res = false;
+            angular.forEach(InfosService.unplayedGrid, function(unplayedCell){
+                if(unplayedCell[0] == l && unplayedCell[1]==c){
+                    res = true
+                }
+            });
+            return res;
+        };
+
+        var bestCells = function(l,c,player){
+            var res = [];
+            if(player === 'blue'){
+                if(isPlaybable(l-1,c+1)){
+                    res.push([l-1,c+1])
+                }
+                if(isPlaybable(l,c+1)){
+                    res.push([l,c+1])
+                }
+            } else {
+                if(isPlaybable(l+1,c-1)){
+                    res.push([l+1,c-1])
+                }
+                if(isPlaybable(l+1,c)){
+                    res.push([l+1,c])
+                }
+            }
+            return res;
+        };
+
+        var otherCells = function(l,c,player){
+            var res = [];
+            if(player === 'blue'){
+                if(isPlaybable(l-1,c)){
+                    res.push([l-1,c])
+                }
+                if(isPlaybable(l+1,c)){
+                    res.push([l+1,c])
+                }
+            } else {
+                if(isPlaybable(l,c-1)){
+                    res.push([l,c-1])
+                }
+                if(isPlaybable(l,c+1)){
+                    res.push([l,c+1])
+                }
+            }
+            return res;
+        };
+
+        var defaultCells = function(l,c,player){
+            var res = [];
+            if(player === 'blue'){
+                if(isPlaybable(l,c-1)){
+                    res.push([l,c-1])
+                }
+                if(isPlaybable(l+1,c-1)){
+                    res.push([l+1,c-1])
+                }
+            } else {
+                if(isPlaybable(l-1,c)){
+                    res.push([l-1,c])
+                }
+                if(isPlaybable(l-1,c+1)){
+                    res.push([l-1,c+1])
+                }
+            }
+            return res;
+        };
+
+        var run = function(player){
+            var cell;
+            if(player === 'blue'){
+                if(bluePreviousCells.length == 0){
+                    var startCell = [Math.floor(GridService.getGridSize()/2),1];
+                    if(isPlaybable(startCell)){
+                        cell=startCell;
+                    } else {
+                        cell=[startCell[0]+1,startCell[1]];
+                    }
+                } else {
+                    for(var i=bluePreviousCells.length - 1; i >= 0; i--){
+                        var currentCell = bluePreviousCells[i];
+                        var l = currentCell[0];
+                        var c =  currentCell[1];
+                        var bests = bestCells(l,c,player);
+                        var others = otherCells(l,c,player);
+                        var defaults = defaultCells(l,c,player);
+                        if(bests.length != 0){
+                            random = Math.floor((Math.random() * bests.length));
+                            cell = [bests[random][0],bests[random][1]];
+                        } else if(others.length != 0){
+                            random = Math.floor((Math.random() * others.length));
+                            cell = [others[random][0],others[random][1]];
+                        } else if(defaults.length != 0){
+                            random = Math.floor((Math.random() * defaults.length));
+                            cell = [defaults[random][0],defaults[random][1]];
+                        } else {
+                            random = Math.floor((Math.random() * InfosService.unplayedGrid.length));
+                            cell=[InfosService.unplayedGrid[random][0],InfosService.unplayedGrid[random][1]];
+                        }
+                    }
+                }
+                bluePreviousCells.push(cell);
+            } else {
+                if(redPreviousCells.length == 0){
+                    startCell = [1,Math.floor(GridService.getGridSize()/2)];
+                    if(isPlaybable(startCell)){
+                        cell=startCell;
+                    } else {
+                        cell=[startCell[0],startCell[1]+1];
+                    }
+                } else {
+                    for(i=redPreviousCells.length - 1; i >= 0; i--){
+                        currentCell = redPreviousCells[i];
+                        l = currentCell[0];
+                        c =  currentCell[1];
+                        bests = bestCells(l,c,player);
+                        others = otherCells(l,c,player);
+                        defaults = defaultCells(l,c,player);
+                        if(bests.length != 0){
+                            random = Math.floor((Math.random() * bests.length));
+                            cell = [bests[random][0],bests[random][1]];
+                        } else if(others.length != 0){
+                            random = Math.floor((Math.random() * others.length));
+                            cell = [others[random][0],others[random][1]];
+                        } else if(defaults.length != 0){
+                            random = Math.floor((Math.random() * defaults.length));
+                            cell = [defaults[random][0],defaults[random][1]];
+                        } else {
+                            random = Math.floor((Math.random() * InfosService.unplayedGrid.length));
+                            cell=[InfosService.unplayedGrid[random][0],InfosService.unplayedGrid[random][1]];
+                        }
+                    }
+                }
+                redPreviousCells.push(cell);
+            }
+            return cell;
+        };
+
+        var indexUnplayedGridCell = function(l,c){
+            var res;
+            angular.forEach(InfosService.unplayedGrid, function(unplayedCell, index){
+                if(unplayedCell[0] == l && unplayedCell[1]==c){
+                    res = index;
+                }
+            });
+            return res;
+        };
+
+        this.play = function(player){
+            var startDate = new Date();
+            var cell = run(player);
+            InfosService.grid[cell[0]][cell[1]] = 'hex-' + player;
+            var index = indexUnplayedGridCell(cell[0],cell[1]);
+            InfosService.unplayedGrid.splice(index,1);
+            InfosService[player+"Played"][cell[0]][cell[1]]="played";
+            var endDate = new Date();
+            var ms = endDate.getTime() - startDate.getTime();
+            InfosService.addMessage("Move done by "+ player +" in " + Math.floor(ms/1000) + "sec " + (ms % 1000) +"ms " +
+            "[" + cell[0] + ","+ cell[1] + "]\n");
+
+        }
+    }]);
+
+    hex.service('PlayService',['InfosService','PlayRandomService','GridService','WinService','PlayPathService',
+        function(InfosService,PlayRandomService,GridService,WinService,PlayPathService){
             var startDate;
             var endDate;
             var init = function(){
@@ -142,9 +316,42 @@
                 }
                 finish();
             };
+            var pathAgainstPath = function(firstPlayer){
+                var gridSize = GridService.getGridSize();
+                init();
+                PlayPathService.init();
+                for (var i=1; i<=(gridSize*gridSize); i++){
+                    var player;
+                    InfosService.addMessage("Move n°" + i);
+                    if(firstPlayer === 'blue'){
+                        if(isOdd(i)){
+                            player = 'blue';
+                        } else {
+                            player = 'red';
+                        }
+                    } else {
+                        if(isOdd(i)){
+                            player = 'red';
+                        } else {
+                            player = 'blue';
+                        }
+                    }
+                    PlayPathService.play(player);
+                    if(WinService.playerWon(player)){
+                        break;
+                    }
+                }
+                finish();
+
+            };
             this.play = function(firstPlayer, bluePlayer, redPlayer){
                 if (bluePlayer === 'random' && redPlayer === 'random'){
                     randomAgainstRandom(firstPlayer);
+                }
+                if(bluePlayer === 'path'){
+                    if(redPlayer === 'path'){
+                        pathAgainstPath(firstPlayer);
+                    }
                 }
             };
         }]);
@@ -281,8 +488,8 @@
             me.gridSize=3;
             me.infos=InfosService;
             me.firstPlayer = 'blue';
-            me.bluePlayer = 'random';
-            me.redPlayer = 'random';
+            me.bluePlayer = 'path';
+            me.redPlayer = 'path';
 
             var init = function(){
                 InfosService.message = "\n---- Logs ----\n\n";
